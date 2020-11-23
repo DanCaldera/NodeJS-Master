a6b149d505f336ad47cc08d5db09bcd26c6ee8dd
Daniel <bdcaldera@gmail.com>
2020-11-23 16:21:29 -0600

    added test folder

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Dependencies
+//* Dependencies
 var math = require('./lib/math')
 
-// Declare the app
+//* Declare the app
 var app = {}
 
 // Init function
+//* Math Functions
 app.getRandomNumber = math.getRandomNumber
 
 module.exports = app
---
+++ test/index.js
@@ -0,0 +1,3 @@
+const bioWolf = require('bio-wolf-js')
+
+console.log(bioWolf.getRandomNumber(1, 9))
