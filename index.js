806b354182063a1cc84b93f9dc7afb7badc86656
Daniel Caldera <dan@MacBook-Air-de-Daniel.local>
2020-11-23 13:09:52 -0600

    third commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var mathLib = require('./lib/math')
+var jokesLib = require('./lib/jokes')
+
+var app = {}
+
+app.config = {
+  timeBetweenJokes: 1000,
+}
+
+app.printJoke = function () {
+  var allJokes = jokesLib.allJokes()
+
+  var numberOfJokes = allJokes.length
+
+  var randomNumber = mathLib.getRandomNumber(1, numberOfJokes)
+
+  var selectedJoke = allJokes[randomNumber - 1]
+
+  console.log(selectedJoke)
+}
+
+app.indefiniteLoop = function () {
+  setInterval(app.printJoke, app.config.timeBetweenJokes)
+}
